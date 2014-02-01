1bd37a7d91a0652909ddb7754200867e954ef43c
Scott Haleen <scott.haleen@gmail.com>
2014-01-31 23:26:10 -0500

    to jekyll

---
+++ _site/script/main.js
@@ -0,0 +1,30 @@
+requirejs.config({
+ baseUrl : '/js',
+ paths : {
+   "zepto" : "zepto.min",
+   "bootstrap" : "bootstrap.min",
+   "knockout" : "knockout-2.2.1",
+   "underscore" : "underscore-min"
+   /* hasher, crossroads, signals */
+ },
+ shim: {
+   "underscore": {
+     "exports" : "_"
+   },
+   "bootstrap" : {
+     deps: ["zepto"],
+     exports: "$"
+   }
+ },
+  config: {
+    text : {
+      env : 'xhr'
+    }
+  }
+});
+
+
+require(["underscore", "zepto"], 
+        function(_, $){
+
+});
